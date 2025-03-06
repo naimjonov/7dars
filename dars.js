ee69d52fcb298fcff7363860afc935f31eb9e87a
Rashidbek <naimjonov@gmail.com>
2025-03-06 21:52:12 +0300

    add file

---
+++ dars.js
@@ -0,0 +1,106 @@
+//1-qoshish
+// function add (a,b){
+//     console.log(a + b)
+// }
+// add(3,5)
+
+//2-ayirish
+// function subtraction(a , b){
+//     console.log(a - b)
+// }
+// subtraction(20,10)
+
+// 3-kopaytirish
+// function multiplication(a , b){
+//     console.log(a * b)
+// }
+// multiplication(2,3)
+
+//4-bolish
+// function division(a , b){
+//     console.log(a / b)
+// }
+// division(6,2)
+
+//5-darajaga kotarish
+// function dagree(a , b){
+//     console.log(a ** b)
+// }
+// dagree(2,3)
+
+//1-misol
+// Ikki sonni ko'paytirib natijasini chiqarish.
+// function multiplication(a , b){
+//     console.log(a * b)
+// }
+// multiplication(5,7)
+
+//2-misol
+// Uchinchi daraja ko'paytirishni hisoblash (masalan, a × b × c).
+// function multiplyThreeNumbers(a,b,c){
+//     let i = a * b * c
+//     console.log(i)
+// }
+// multiplyThreeNumbers(2,3,4)
+
+//3-misol 
+// To'rt sonning ko'paytmasini hisoblash.
+// function multiplication(a,b,c,d){
+//     let i = a * b * c * d
+//     console.log(i)
+// }
+// multiplication(5,2,9,7)
+
+//4-misol
+// Ikkita musbat sonning ko'paytmasini chiqarish.
+// function multiplication(a,b){
+//     let i = a * b
+//     console.log(i)
+// }
+// multiplication(56,4)
+
+//5-misol
+// Ikkita manfiy sonning ko'paytmasini chiqarish.
+// function manfiy(a, b){
+//     let i = a * b
+//     console.log(i)
+// }
+// manfiy(-2,-9)
+
+//6-misol
+// Musbat va manfiy sonlarning ko'paytmasini chiqarish.
+// function aralash(a,b){
+//     let i = a * b
+//     console.log(i)
+// }
+// aralash(5,-5)
+
+//7-misol
+// Sonlarni yig'indisi va ko'paytmasini birga chiqarish.
+// function yigindi(a,b){
+//     let i = a * b
+//     let j = a + b
+//     console.log("kopaytma" +i,"yigindi" +j)
+// }
+// yigindi(3,4)
+
+//8-misol
+// Ikkita sonning ko'paytmasini hisoblash va natijani 2 ga bo'lish.
+// function n(a,b){
+//     let i = a * b / 2
+//     console.log(i)
+// }
+// n(4,4)
+
+//9-misol
+// A va B sonlarini ko'paytirib, ular orasidagi foizni hisoblash
+// function n(a, b) {
+//     let i = a * b
+//     let j = (i / a) * 100
+//     console.log(i,j)
+// }
+// n(5, 8)
+
+
+
+
